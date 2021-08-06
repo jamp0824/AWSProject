5f66596ebc116bea8fdb2b14aca555b54c32eefd
jun onAn <loneyjune@naver.com>
2021-08-06 18:25:45 +0900

    commit rest of source about o2auth

--- src/test/java/com/project/aws/web/HelloControllerTest.java
+++ src/test/java/com/project/aws/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.project.aws.web;
 
+import com.project.aws.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,12 +16,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+             excludeFilters = {
+             @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+             })
+
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloIsReturn() throws Exception{
         String Hello = "hello";
@@ -27,6 +36,7 @@ public class HelloControllerTest {
                 .andExpect(content().string("hello"));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDtoIsReturn() throws Exception{
         String name = "hello";
